3869a38fd5263a724ee0107ee9505b291402fbf8
leofrota <leonardo.matsuno.frota@gmail.com>
2020-03-25 17:19:44 -0300

    .
    .

---
+++ datasus.R
@@ -0,0 +1,11 @@
+### Chamando pacotes
+library(dplyr)
+library(datasus)
+
+
+### teste
+
+teste = sinasc_nv_uf(uf = "ms",
+                     periodo = c(2011:2015),
+                     coluna = "Ano do nascimento")
+
